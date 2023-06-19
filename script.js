e7322cc3c4952ce6d9e10a1b51d537f3bff0fce4
Akshay Chaudhary <124782758+AkshayChaudharyctrl@users.noreply.github.com>
2023-06-19 10:17:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+ // Get references to the counter element and the increment button
+    const counterElement = document.getElementById("counter");
+    const incrementBtn = document.getElementById("incrementBtn");
+
+    // Add event listener to the increment button
+    incrementBtn.addEventListener("click", () => {
+      // Get the current value of the counter
+      const currentValue = parseInt(counterElement.innerText);
+
+      // Display an alert with the current value before incrementing
+      alert("Current value: " + currentValue);
+
+      // Increment the counter
+      counterElement.innerText = currentValue + 1;
+    });
